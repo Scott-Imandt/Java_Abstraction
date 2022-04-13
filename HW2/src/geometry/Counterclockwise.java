18f3b383761ed78b30d06834c29fe0c6d4de6b63
Scott-Imandt <scottimandt@gmail.com>
2022-04-13 01:21:05 -0400

    Added Counterclockwise.java to commit

---
+++ HW2/src/geometry/Counterclockwise.java
@@ -0,0 +1,42 @@
+package geometry;
+
+import java.util.Comparator;
+
+public class Counterclockwise implements Comparator<Point>{
+
+	// implement serializable
+	
+	
+	@Override
+	public int compare(Point o1, Point o2) {
+		
+		if(o1.getX()>o2.getX()) {
+			return 1;
+		}
+		
+		else if(o1.getX()== o2.getX()) {
+			
+			if(o1.getY()>o2.getY()) {
+				return 1;
+			}
+			
+			else if(o1.getY()== o2.getY()) {
+				return 0;
+				
+			}
+			
+			else if(o1.getY() < o2.getY()) {
+				return -1;
+			}
+	
+		}
+		
+		else if(o1.getX() < o2.getX()) {
+			return -1;
+		}
+		
+		return -1;
+		
+	}
+
+}
